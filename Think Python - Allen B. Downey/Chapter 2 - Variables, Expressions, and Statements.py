bf935a02c78e77e41f1d7539f3d921a08a8bbf33
Güneş Evitan <gunesevitan@gmail.com>
2018-02-15 18:51:23 +0300

    Chapter 2 final changes

--- Think Python - Allen B. Downey/Chapter 2 - Variables, Expressions, and Statements.py
+++ Think Python - Allen B. Downey/Chapter 2 - Variables, Expressions, and Statements.py
@@ -80,25 +80,18 @@ print(delimiter * 5)  # '.....', string
 
 # Practice using the Python interpreter as a calculator:
 
-# The volume of a sphere with radius r is 4/3 π r3.
-# What is the volume of a sphere with radius 5? Hint: 392.7 is wrong!
-
-# Suppose the cover price of a book is $24.95, but bookstores get a 40% discount.
-# Shipping costs $3 for the first copy and 75 cents for each additional copy.
-#  What is the total wholesale cost for 60 copies?
-
-# If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile),
-# then 3 miles at tempo (7:12 per mile) and 1 mile at easy pace again, what time do I get home for breakfast?
-
 # Answer:
 
-# Question 1:
+# 1. The volume of a sphere with radius r is 4/3 π r3.
+#    What is the volume of a sphere with radius 5? Hint: 392.7 is wrong!
 
 from math import pi
 
 (4.0 / 3.0) * pi * 5**3  # 523.5987755982989
 
-# Question 2:
+# 2. Suppose the cover price of a book is $24.95, but bookstores get a 40% discount.
+#    Shipping costs $3 for the first copy and 75 cents for each additional copy.
+#    What is the total wholesale cost for 60 copies?
 
 quantity = 60
 cover_price = 24.95
@@ -107,7 +100,8 @@ cost = (cover_price * quantity) - (cover_price * quantity) * discount
 shipping = 3 + (0.75 * quantity)
 total_cost = cost + shipping  # 946.199999999
 
-# Question 3:
+# 3. If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile),
+#    then 3 miles at tempo (7:12 per mile) and 1 mile at easy pace again, what time do I get home for breakfast?
 
 from datetime import datetime, timedelta
 
