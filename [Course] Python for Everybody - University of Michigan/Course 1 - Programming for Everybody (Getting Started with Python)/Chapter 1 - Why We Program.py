827a5d8aa7589c9b57d3edbd71e29641090f130e
Güneş Evitan <gunesevitan@gmail.com>
2018-02-20 09:32:27 +0300

    Chapter 1 Refactor

--- [Course] Python for Everybody - University of Michigan/Course 1 - Programming for Everybody (Getting Started with Python)/Chapter 1 - Why We Program.py
+++ [Course] Python for Everybody - University of Michigan/Course 1 - Programming for Everybody (Getting Started with Python)/Chapter 1 - Why We Program.py
@@ -1,77 +1,87 @@
 # Chapter One Quiz
 
 
-
-#  1. When Python is running in the interactive mode and displaying the chevron prompt (>>>)
-#     - what question is Python asking you?
-
-
-#     What would you like to do?
+# 1.
+# When Python is running in the interactive mode and displaying the chevron prompt (>>>)
+# what question is Python asking you?
 
 
-#  2. What will the following program print out:
-
-#     >>> x = 15;
-#     >>> x = x + 5;
-#     >>> print x
+# What would you like to do?
 
 
-#     20
+# 2.
+# What will the following program print out:
+
+x = 15;
+x = x + 5;
+print(x)
 
 
-#  3. Python scripts (files) have names that end with:
+# 20
 
 
-#     .py
+# 3.
+# Python scripts (files) have names that end with:
 
 
-#  4. Which of these words are reserved words in Python ?
+# .py
 
 
-#     if
-#     break
+# 4.
+#  Which of these words are reserved words in Python ?
 
 
-#  5. What is the proper way to say “good-bye” to Python?
+# if
+# break
 
 
-#     quit()
+# 5.
+# What is the proper way to say “good-bye” to Python?
 
 
-#  6. Which of the parts of a computer actually executes the program instructions?
+# quit()
 
 
-#     Central Processing Unit
+# 6.
+# Which of the parts of a computer actually executes the program instructions?
 
 
-#  7. What is "code" in the context of this course?
+# Central Processing Unit
 
 
-#     A sequence of instructions in a programming language
+# 7.
+# What is "code" in the context of this course?
 
 
-#  8. A USB memory stick is an example of which of the following components of computer
-#     architecture?
+# A sequence of instructions in a programming language
 
 
-#     Secondary Memory
+# 8.
+# A USB memory stick is an example of which of the following components of computer
+# architecture?
 
 
-#  9. What is the best way to think about a "Syntax Error" while programming?
+# Secondary Memory
 
 
-#     The computer did not understand the statement that you entered
+# 9.
+# What is the best way to think about a "Syntax Error" while programming?
 
 
-#  10. Which of the following is not one of the programming patterns covered in Chapter 1?
+# The computer did not understand the statement that you entered
 
 
-#      Random steps
+#10.
+# Which of the following is not one of the programming patterns covered in Chapter 1?
 
+
+# Random steps
+
+
+#--------------------------------------------------------------------------------------------------
 
 
 # Chapter One Assignments
-
 
 
 # Assignment Write Hello World
