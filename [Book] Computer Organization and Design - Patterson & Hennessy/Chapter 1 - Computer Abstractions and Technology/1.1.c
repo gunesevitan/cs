13e817acb300d021cc1938b1b633088b1512e277
Güneş Evitan <gunesevitan@gmail.com>
2018-03-03 11:11:05 +0300

    1.1

---
+++ [Book] Computer Organization and Design - Patterson & Hennessy/Chapter 1 - Computer Abstractions and Technology/1.1.c
@@ -0,0 +1,28 @@
+/*
+
+ * 1.1
+ * Aside from the smart cell phones used by a billion people, list and 
+ * describe four other types of computers.
+ 
+ 
+ * Servers: They are large computers that allow various users to access 
+ * software applications from any computer through a network. Also, servers 
+ * are used by other computers within an organization and have large amount 
+ * of memory, terabytes.
+
+ * Embedded Computers: They are single task computers that perform a single 
+ * task with a large performance. In other words, they are low power consumption,
+ * small size, rugged operating ranges, and low per-unit-cost. For instance,
+ * ATM machines, CD players, microprocessors, etc.
+
+ * Personal Computers: They are the sophisticated version of smartphones. 
+ * They are faster than smartphones, have higher computer power, and they 
+ * are single user computers.
+ 
+ * Super Computers: A super computer is a computer with high-level computational 
+ * capacity compared to a general-purpose computer. Their memory are hundreds of 
+ * terabytes and the performance of this computers are measured in foating-point 
+ * operations per second (FLOPS). Because of their efficiency and performance,
+ * they are very expensive.
+ 
+ */
