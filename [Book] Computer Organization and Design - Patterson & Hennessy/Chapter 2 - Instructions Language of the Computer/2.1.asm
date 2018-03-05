39fc7939a6b6af66bf1d38e8de79330acea71cab
Güneş Evitan <gunesevitan@gmail.com>
2018-03-05 12:33:30 +0300

    2.1

---
+++ [Book] Computer Organization and Design - Patterson & Hennessy/Chapter 2 - Instructions Language of the Computer/2.1.asm
@@ -0,0 +1,25 @@
+/*
+
+ * 2.1 
+ 
+ 
+ * For the following C statement, what is the corresponding
+ * MIPS assembly code? Assume that the variables f, g, h , 
+ * and i are given and could be considered 32-bit integers 
+ * as declared in a C program. Use a minimal number of MIPS
+ * assembly instructions. 
+ 
+ * f = g + (h – 5);
+ 
+ */
+
+
+# Answer
+
+
+# h -> $t0
+# g -> $t1
+# s -> $s1
+
+addi	$t0, $t0, -5	# h = h - 5
+add		$s1, $t1, $t0	# f = g + h
