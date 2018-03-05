f31c354bd9e6df92b0af4ebe34e6f3ad2cfb225c
Güneş Evitan <gunesevitan@gmail.com>
2018-03-05 20:03:40 +0300

    2.4 r

--- [Book] Computer Organization and Design - Patterson & Hennessy/Chapter 2 - Instructions Language of the Computer/2.4.asm
+++ [Book] Computer Organization and Design - Patterson & Hennessy/Chapter 2 - Instructions Language of the Computer/2.4.asm
@@ -34,15 +34,15 @@
 # A[0] -> $s6
 # B[0] -> $s7
 
-sll	$t0, $s0, 2 	# $t0 = f * 4 
-add	$t0, $s6, $t0	# $t0 = &A[f] 
-sll	$t1, $s1, 2 	# $t1 = g * 4 
-add	$t1, $s7, $t1	# $t1 = &B[g] 
-lw	$s0, 0($t0) 	# f = A[f] 
-addi $t2, $t0, 4 	# A[f + 1]
-lw	$t0, 0($t2) 	# $t0 = A[f + 1]
-add	$t0, $t0, $s0	# $t0 = A[f + 1] + A[f]
-sw	$t0, 0($t1) 	# B[g] = $t0
+sll 	$t0, $s0, 2 	# $t0 = f * 4 
+add 	$t0, $s6, $t0	# $t0 = &A[f] 
+sll 	$t1, $s1, 2 	# $t1 = g * 4 
+add 	$t1, $s7, $t1	# $t1 = &B[g] 
+lw  	$s0, 0($t0) 	# f = A[f] 
+addi	$t2, $t0, 4 	# A[f + 1]
+lw  	$t0, 0($t2) 	# $t0 = A[f + 1]
+add 	$t0, $t0, $s0	# $t0 = A[f + 1] + A[f]
+sw  	$t0, 0($t1) 	# B[g] = $t0
  
 # f = A[f]
 # f = A[f + 1] + A[f]
