0ce470db44a20987e07914aa1fb8ba8aaaffc2c5
Güneş Evitan <gunesevitan@gmail.com>
2018-03-06 22:06:59 +0300

    2.27

--- [Book] Computer Organization and Design - Patterson & Hennessy/Chapter 2 - Instructions Language of the Computer/2.24.asm
+++ [Book] Computer Organization and Design - Patterson & Hennessy/Chapter 2 - Instructions Language of the Computer/2.24.asm
@@ -1,30 +1,14 @@
-# 2.27
+# 2.24
  
-# Translate the following C code to MIPS assembly code. Use a minimum 
-# number of instructions. Assume that the values of a, b, i, and j are
-# in registers $s0, $s1, $t0, and $t1, respectively. Also, assume that 
-# register $s2 holds the base address of the array D.
-
-for(i=0; i<a; i++)
- for(j=0; j<b; j++)
-  D[4*j] = i + j;
+# Suppose the program counter (PC) is set to 0x2000 0000. Is it 
+# possible to use the jump (j) MIPS assembly instruction to set 
+# the PC to the address as 0x4000 0000? Is it possible to use the
+# branch-on-equal (beq) MIPS assembly instruction to set the PC 
+# to this same address?
 
 
 # Answer
 
 
-	add	$t0, $0, $0 	# i = 0
-L1:	slt	$t2, $t0, $s0	# i < a
-	beq	$t2, $0, EXIT	# $t2 == 0, go to EXIT
-	add	$t1, $0, $0 	# j = 0
-L2:	slt	$t2, $t1, $s1	# j < b
-	beq	$t2, $0, L3 	# if $t2 == 0, go to L3
-	add	$t2, $t0, $t1	# i+j
-	sll	$t4, $t1, 2 	# $t4 = 4*j
-	add	$t3, $t4, $s2	# $t3 = &D[4*j]
-	sw	$t2, 0($t3) 	# D[4*j] = i+j
-	addi	$t1, $t1, 1 # j = j+1
-	j	L2
-L3:	addi	$t0, $t0, 1 # i = i+1
-	j	L1
-EXIT
+# Jump instruction can jump to 28 bits and
+# Branch instruction can branch to 18 bits.
