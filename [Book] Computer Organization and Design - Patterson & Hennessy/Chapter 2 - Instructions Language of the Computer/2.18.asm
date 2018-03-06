4df3309649681312293ffc5d80fa2a927faaa1e2
Güneş Evitan <gunesevitan@gmail.com>
2018-03-06 17:26:07 +0300

    2.18

---
+++ [Book] Computer Organization and Design - Patterson & Hennessy/Chapter 2 - Instructions Language of the Computer/2.18.asm
@@ -0,0 +1,29 @@
+# 2.18
+ 
+# Assume that we would like to expand the MIPS register file to 128 registers
+# and expand the instruction set to contain four times as many instructions.
+
+# 2.18.1
+
+# How would this affect the size of each of the bit fields in the 
+# R-type instructions?
+
+# 2.18.2
+
+# How would this affect the size of each of the bit fields in the 
+# I-type instructions?
+
+# 2.18.3
+
+# How could each of the two proposed changes decrease the size of an 
+# MIPS assembly program? On the other hand, how could the proposed change 
+# increase the size of an MIPS assembly program?
+
+
+# Answer
+
+
+# 2.18.1
+
+# opcode | rs | rt | rd | shamt | funct
+# 	20  	7	7	 7  	3		20 Bits
