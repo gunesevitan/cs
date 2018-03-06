3a83c1afa5e9a4620e75808483bb17eae1ea1c6a
Güneş Evitan <gunesevitan@gmail.com>
2018-03-06 19:04:28 +0300

    2.21

---
+++ [Book] Computer Organization and Design - Patterson & Hennessy/Chapter 2 - Instructions Language of the Computer/2.21.asm
@@ -0,0 +1,12 @@
+# 2.21
+ 
+# Provide a minimal set of MIPS instructions that may be used to implement 
+# the following pseudoinstruction:
+
+# not $t1, $t2 // bit-wise invert
+
+
+# Answer
+
+
+nor	$t1, $t2, $zero
