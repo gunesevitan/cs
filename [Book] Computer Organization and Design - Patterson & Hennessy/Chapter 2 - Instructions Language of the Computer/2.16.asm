c47300fde444dcc1c35025990b8718feb03cf3bd
Güneş Evitan <gunesevitan@gmail.com>
2018-03-06 12:13:59 +0300

    2.16

---
+++ [Book] Computer Organization and Design - Patterson & Hennessy/Chapter 2 - Instructions Language of the Computer/2.16.asm
@@ -0,0 +1,15 @@
+# 2.15
+ 
+# Provide the type, assembly language instruction, and binary representation 
+# of instruction described by the following MIPS fields:
+
+# op=0, rs=3, rt=2, rd=3, shamt=0, funct=34
+
+
+# Answer
+
+
+# opcode | rs | rt | rd | shamt | funct
+# 000000 00011 00010 00011 00000 100010
+	
+sub $v1, $v1, $v0	# R-type sub instruction
