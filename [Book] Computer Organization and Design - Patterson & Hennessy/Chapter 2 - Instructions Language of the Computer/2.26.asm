ee0dce9f6e93bd71f106df8d8be1bcf73f9d8e7c
Güneş Evitan <gunesevitan@gmail.com>
2018-03-06 21:34:28 +0300

    refactors

--- [Book] Computer Organization and Design - Patterson & Hennessy/Chapter 2 - Instructions Language of the Computer/2.26.asm
+++ [Book] Computer Organization and Design - Patterson & Hennessy/Chapter 2 - Instructions Language of the Computer/2.26.asm
@@ -2,11 +2,11 @@
  
 # Consider the following MIPS loop:
 
-LOOP:	slt $t2, $0, $t1
-		beq $t2, $0, DONE
-		subi $t1, $t1, 1
-		addi $s2, $s2, 2
-		j LOOP
+LOOP:slt $t2, $0, $t1
+	beq $t2, $0, DONE
+	subi $t1, $t1, 1
+	addi $s2, $s2, 2
+	j LOOP
 DONE:
 
 # 2.26.1
