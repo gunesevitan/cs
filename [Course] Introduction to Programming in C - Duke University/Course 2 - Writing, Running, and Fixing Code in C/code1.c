73062f38fa05fce5e93237297b0eca83eaac49d8
Güneş Evitan <gunesevitan@gmail.com>
2018-09-04 11:13:52 +0300

    course 2 week 1 finished

---
+++ [Course] Introduction to Programming in C - Duke University/Course 2 - Writing, Running, and Fixing Code in C/code1.c
@@ -0,0 +1,18 @@
+int max (int num1, int num2) {
+  if( num1 > num2){
+    return num1;
+  }else{
+    return num2;
+  }
+}
+
+int main(void) {
+  printf("max(42, -69) is %d\n", max(42, -69));
+  printf("max(33, 0) is %d\n", max(33, 0));
+  printf("max(0x123456, 123456) is %d\n", max(0x123456, 123456));
+  printf("max(0x451215AF, 0x913591AF) is %d\n", max(0x451215AF, 0x913591AF));
+  
+  return 0;
+}
+
+
---
+++ [Course] Introduction to Programming in C - Duke University/Course 2 - Writing, Running, and Fixing Code in C/code2.c
@@ -0,0 +1,26 @@
+int printTriangle(int size) {
+  int starCount = 0;
+
+  for(int i = 0;i < size;i++){
+    for(int j = 0;j < i+1; j++){
+      printf("*");
+      starCount += 1;
+    }
+    printf("\n");
+  }
+  return starCount;
+}
+
+int main(void) {
+  int numStars;
+  printf("Here is a triangle with height 4\n");
+  numStars = printTriangle(4);
+  printf("That triangle had %d total stars\n", numStars);
+  printf( "Here is a triangle with height 7\n");
+  numStars = printTriangle(7);
+  printf( "That triangle had %d total stars\n",numStars); 
+  return 0;
+}
+
+
+
