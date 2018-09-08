f77c694c71e6380d66f3d91e091dd6a99b39bef5
Güneş Evitan <gunesevitan@gmail.com>
2018-09-08 17:21:58 +0300

    course 3 week 1

--- [Course] Introduction to Programming in C - Duke University/Course 3 - Pointers, Arrays, and Recursion/Week 1.c
+++ [Course] Introduction to Programming in C - Duke University/Course 3 - Pointers, Arrays, and Recursion/Week 1.c
@@ -27,3 +27,56 @@ int * b = 42;
 
 // Give me an arrow pointing at
 
+
+// Week 1 - Practice Quiz: Pointers Conceptually
+
+
+// 1.
+// We represent pointers conceptually as arrows, but everything is a number. What number is an 
+// arrow?
+
+
+// An address in memory
+
+
+// 2.
+// Where in memory are local variables stored?
+
+
+// Stack
+
+
+// 3.
+// A pointer with value NULL has numeric value 0. What is placed at address 0?
+
+
+// Nothing
+
+
+// Week 1 - Practice Quiz: Pointers to Sophisticated Types
+
+
+// 1.
+// Assuming a is a pointer to a struct with field x, which one of the following has the same
+// semantics (meaning) as (*a).x ?
+
+
+// a->x
+
+
+// 2.
+// If we write &e, then e must be...
+
+
+// An lvalue
+
+
+// 3.
+// For the following code:
+
+const int * p = &x;
+
+// Which one of the following will result in a compiler error?
+
+
+// *p = 5;
