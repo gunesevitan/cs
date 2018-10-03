6a6476370f3a89a5350176cdd0920ec628db1621
Güneş Evitan <gunesevitan@gmail.com>
2018-10-03 09:39:20 +0300

    Course 2 Week 2

--- [Course] IBM Data Science Professional Certificate/Course 2 - Open Source tools for Data Science/Week 1.py
+++ [Course] IBM Data Science Professional Certificate/Course 2 - Open Source tools for Data Science/Week 1.py
@@ -47,3 +47,49 @@
 
 
 # True
+
+
+# Week 1 - Quiz: Quiz 2 - Jupyter Notebooks
+
+
+# 1.
+# What can you write in Jupyter Notebooks? Select all that apply:
+
+
+# Code to be executed in one of the kernels (e.g. Python, R or Scala)
+# Stylized text in a format called "Markdown".
+# HTML code -- HTML can be written and rendered via Markdown cells.
+
+
+# 2.
+# What were Jupyter notebooks called before the name was changed to “Jupyter”?
+
+
+# IPython notebooks
+
+
+# 3.
+# Which of the following can specifically enable you to leverage big data with Jupyter
+# Notebooks? 
+
+
+# Apache Spark
+
+
+# 4.
+# Which of the following options are TRUE? Select all that apply.
+
+
+# You can download and save Jupyter Notebooks as .ipynb files.
+# You can change kernels within a Jupyter Notebook (e.g., to R or Python or Scala).
+# You can connect to databases from within Jupyter Notebooks.
+
+
+# 5.
+# True or False? Although you can change the kernel of the Jupyter Notebook between different 
+# programming languages (e.g., Python, R, Scala), you cannot use multiple kernels within the
+# same Jupyter notebook (e.g., running Python, R and Scala within the same notebook).
+
+
+# True
+
