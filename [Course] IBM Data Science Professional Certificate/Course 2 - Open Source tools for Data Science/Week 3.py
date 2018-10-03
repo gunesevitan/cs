f51199562787b74b236655311d50e3e0fde9dc98
Güneş Evitan <gunesevitan@gmail.com>
2018-10-03 11:57:42 +0300

    Course 2 Week 3

---
+++ [Course] IBM Data Science Professional Certificate/Course 2 - Open Source tools for Data Science/Week 3.py
@@ -0,0 +1,45 @@
+# Week 3 - Quiz: Quiz 5 - IBM Watson Studio
+
+
+# 1.
+# What is the key difference between Cognitive Class Labs and IBM Watson Studio (Data Science
+# Experience)?
+
+
+# IBM Watson Studio is an enterprise-ready environment with scalability in mind, 
+# whereas Cognitive Class Labs is primarily a learning environment.
+
+
+# 2.
+# True or False? The only way to add notebooks to your project on IBM Watson Studio is to 
+# either create an empty notebook, or upload a notebook from your computer.
+
+
+# False
+
+
+# 3.
+# Which of the follow options are correct? Select all that apply:
+
+
+# You can add collaborators to projects
+# Collaborators on a project can add comments to notebooks.
+# You can use Python 3, R or Scala with Jupyter Notebooks on Watson Studio
+
+
+# 4.
+# In the upper right-hand corner of a Jupyter Notebook is the kernel interpreter (e.g., Python 3).
+# Next to it is a circle. What does it mean if the circle is a full, black circle and what should you
+# do?
+
+
+# A black circle means that the kernel is not ready to execute more code. You can 
+# either wait, interrupt, or try to restart the kernel.
+
+
+# 5.
+# True or False? RStudio is available on IBM Watson Studio
+
+
+# True
+
