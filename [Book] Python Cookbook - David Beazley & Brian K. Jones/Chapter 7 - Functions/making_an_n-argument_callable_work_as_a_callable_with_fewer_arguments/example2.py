ae84463f0f17f2225c2a1d28d5992c06eb0b5104
Güneş Evitan <gunesevitan@gmail.com>
2018-10-25 14:19:32 +0300

    Python Cookbook

---
+++ [Book] Python Cookbook - David Beazley & Brian K. Jones/Chapter 7 - Functions/making_an_n-argument_callable_work_as_a_callable_with_fewer_arguments/example2.py
@@ -0,0 +1,24 @@
+# Using partial to supply extra arguments to a callback function
+
+import functools
+
+def output_result(result, log=None):
+    if log is not None:
+        log.debug('Got: %r', result)
+
+# A sample function
+def add(x, y):
+    return x + y
+
+if __name__ == '__main__':
+    import logging
+    from multiprocessing import Pool
+    from functools import partial
+
+    logging.basicConfig(level=logging.DEBUG)
+    log = logging.getLogger('test')
+
+    p = Pool()
+    p.apply_async(add, (3, 4), callback=functools.partial(output_result, log=log))
+    p.close()
+    p.join()
